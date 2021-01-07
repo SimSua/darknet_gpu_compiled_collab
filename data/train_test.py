0d4f1f632398976578086a99f84598670de94112
Simon Suard <simon.suard@cpe.fr>
2021-01-07 15:28:11 +0100

    better script

--- data/train_test.py
+++ data/train_test.py
@@ -3,21 +3,23 @@ import random
 
 directory = os.path.abspath(os.curdir)+"/obj/"
 images = []
+train_filename = 'train_animals.txt'
+test_filename = 'test_animals.txt'
 for i in os.listdir(directory):
     if '.txt' not in i:
       images.append(i)
 random.shuffle(images)  
 print(len(images))
-separation = (len(images)/100)*80
+separation = int((len(images)/100)*80)
 print(separation)
 train = images[:separation]
 test = images[separation:]
 
 print(len(test)+len(train))
 
-t = open('train.txt', 'w')
+t = open(train_filename, 'w')
 for i in train:
-    t.write(str(i)+'\n')
-te = open('test.txt', 'w')
+    t.write(directory+str(i)+'\n')
+te = open(test_filename, 'w')
 for i in test:
-    te.write(str(i)+'\n')
+    te.write(directory+str(i)+'\n')
